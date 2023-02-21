f0b06aa07c64673fa602be5daeaa87894a166506
GasinskiN <gasinskinorman@gmail.com>
2023-02-21 23:29:35 +0100

    Added images and a singular sound

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function handleClick() {
-    alert("i got clicked");
+    var drum = new Audio("sounds/crash.mp3");
+    drum.play();
 }
 
 document.querySelectorAll(".drum").forEach(el => el.addEventListener("click", handleClick));
